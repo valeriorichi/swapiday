f68e21adb9b6588cf12fce31ff39f53c9d9c68d4
Quinn Lord <quinndolin404@gmail.com>
2023-04-27 15:00:06 +0100

    add homecard

---
+++ components/HomeCard.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import React from "react";
+
+function HomeCard() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>I'm a card with home information !!</Text>
+    </View>
+  );
+}
+
+export default HomeCard;

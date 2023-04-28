069b735b63df48d9c91eccc65992c6b89db2aa53
David Maizil <davidmaizil@Davids-MBP.broadband>
2023-04-28 10:04:12 +0100

    added landing page

--- screens/Landing.jsx
+++ screens/Landing.jsx
@@ -1,10 +1,30 @@
-import { Text, View } from "react-native";
+import { Text, View, Image, TouchableOpacity } from "react-native";
 import React from "react";
 
 function Landing() {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Landing page!</Text>
+      <Image
+        source={require("../assets/Swapiday-Big-Logo.png")}
+        style={{ width: 300, height: 300 }}
+        resizeMode="contain"
+      />
+      <View style={{ marginTop: 20 }}>
+        <TouchableOpacity
+          style={{
+            backgroundColor: "#7bd060",
+            padding: 10,
+            borderRadius: 5,
+            width: 100,
+          }}
+        >
+          <Text
+            style={{ color: "black", fontWeight: "bold", textAlign: "center" }}
+          >
+            Login
+          </Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }

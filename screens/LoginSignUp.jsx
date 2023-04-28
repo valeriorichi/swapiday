b3a679fa7fdef8c40346c3480202265b63f30c9e
David Maizil <davidmaizil@Davids-MBP.broadband>
2023-04-28 11:24:50 +0100

    added HeaderLogo in all pages

--- screens/LoginSignUp.jsx
+++ screens/LoginSignUp.jsx
@@ -1,11 +1,16 @@
 import { Text, View } from "react-native";
 import React from "react";
+import LogoHeader from "./LogoHeader";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 function LoginSignUp() {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Login Sign Up !</Text>
-    </View>
+    <SafeAreaView style={{ flex: 1 }}>
+      <LogoHeader />
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <Text>Login Sign Up !</Text>
+      </View>
+    </SafeAreaView>
   );
 }
 
--- screens/UserProfile.jsx
+++ screens/UserProfile.jsx
@@ -1,11 +1,17 @@
 import { Text, View } from "react-native";
 import React from "react";
+import LogoHeader from "./LogoHeader";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 function UserProfile() {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>User profile !</Text>
-    </View>
+    <SafeAreaView style={{ flex: 1 }}>
+      <LogoHeader />
+
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <Text>User profile !</Text>
+      </View>
+    </SafeAreaView>
   );
 }
 

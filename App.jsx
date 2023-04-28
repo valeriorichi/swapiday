3fc4611e8f76e56e68a98b54fbcb719fe82b196b
adaml98 <47745325+adaml98@users.noreply.github.com>
2023-04-28 15:50:23 +0100

    create static search page

--- App.jsx
+++ App.jsx
@@ -1,20 +1,21 @@
-import { StyleSheet } from 'react-native';
-import { signInWithEmailAndPassword } from 'firebase/auth';
-import { auth } from './firebase';
-import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Landing from './screens/Landing';
-import HomeList from './screens/HomeList';
-import AddHome from './screens/AddHome';
-import LoginSignUp from './screens/LoginSignUp';
-import UserProfile from './screens/UserProfile';
-import { AuthProvider, useAuth } from './contexts/AuthContext';
-import DebugAccount from './screens/DebugAccount';
+import { StyleSheet } from "react-native";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "./firebase";
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Landing from "./screens/Landing";
+import HomeList from "./screens/HomeList";
+import AddHome from "./screens/AddHome";
+import LoginSignUp from "./screens/LoginSignUp";
+import UserProfile from "./screens/UserProfile";
+import { AuthProvider, useAuth } from "./contexts/AuthContext";
+import DebugAccount from "./screens/DebugAccount";
 import {
   Provider as PaperProvider,
   MD3LightTheme as DefaultTheme,
-} from 'react-native-paper';
+} from "react-native-paper";
+import Search from "./screens/Search";
 
 const Tab = createBottomTabNavigator();
 
@@ -25,6 +26,7 @@ export default function App() {
         <NavigationContainer>
           <Tab.Navigator>
             <Tab.Screen name="Landing" component={Landing} />
+            <Tab.Screen name="Search" component={Search} />
             <Tab.Screen name="Homes" component={HomeList} />
             <Tab.Screen name="List a home" component={AddHome} />
             <Tab.Screen name="Login/Sign up" component={LoginSignUp} />
@@ -40,8 +42,8 @@ const theme = {
   ...DefaultTheme,
   colors: {
     ...DefaultTheme.colors,
-    primary: 'tomato',
-    secondary: 'yellow',
+    primary: "tomato",
+    secondary: "yellow",
   },
 };
 

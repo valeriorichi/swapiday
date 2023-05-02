e9b170f468539127b65aa7ae1cfbdba835bb2e62
David Maizil <davidmaizil@Davids-MBP.broadband>
2023-05-02 15:39:07 +0100

    added adding available dates and removing them from a list

--- screens/UpdateListHouse.jsx
+++ screens/UpdateListHouse.jsx
@@ -1,59 +1,94 @@
 import React, { useState } from "react";
-import { View } from "react-native";
-import { TextInput } from "react-native-paper";
-import { Calendar } from "react-native-calendars";
+import { View, Text, Button, StyleSheet } from "react-native";
+import DateTimePicker from "@react-native-community/datetimepicker";
 
 const UpdateListHouse = () => {
-  const [startDate, setStartDate] = useState(null);
-  const [endDate, setEndDate] = useState(null);
-  const [markedDates, setMarkedDates] = useState({});
+  const [fromDate, setFromDate] = useState(new Date());
+  const [untilDate, setUntilDate] = useState(new Date());
+  const [dateList, setDateList] = useState([]);
 
-  const handleDayPress = (day) => {
-    const newMarkedDates = { ...markedDates };
-    if (startDate == null) {
-      newMarkedDates[day.dateString] = { startingDay: true, color: "blue" };
-      setStartDate(day);
-      setMarkedDates(newMarkedDates);
-    } else if (endDate == null && day.dateString > startDate.dateString) {
-      let date = new Date(startDate.year, startDate.month - 1, startDate.day);
-      while (date <= new Date(day.year, day.month - 1, day.day)) {
-        newMarkedDates[date.toISOString().slice(0, 10)] = {
-          color: "blue",
-          textColor: "white",
-        };
-        date.setDate(date.getDate() + 1);
-      }
-      newMarkedDates[day.dateString] = { endingDay: true, color: "blue" };
-      setEndDate(day);
-      setMarkedDates(newMarkedDates);
-    } else {
-      setStartDate(day);
-      setEndDate(null);
-      setMarkedDates({
-        [day.dateString]: { startingDay: true, color: "blue" },
-      });
-    }
+  const handleAddDate = () => {
+    setDateList([...dateList, { from: fromDate, until: untilDate }]);
   };
 
+  const handleDeleteDate = (index) => {
+    const newList = [...dateList];
+    newList.splice(index, 1);
+    setDateList(newList);
+  };
+
+  const formatDate = (date) => {
+    const options = { year: "numeric", month: "long", day: "numeric" };
+    return date.toLocaleDateString("en-US", options);
+  };
+
+  const dateListItems = dateList.map((date, index) => (
+    <View key={index} style={styles.dateListItem}>
+      <Text style={styles.dateText}>From: {formatDate(date.from)}</Text>
+      <Text style={styles.dateText}>Until: {formatDate(date.until)}</Text>
+      <Button title="Delete" onPress={() => handleDeleteDate(index)} />
+    </View>
+  ));
+
   return (
-    <View>
-      <Calendar
-        markedDates={markedDates}
-        onDayPress={handleDayPress}
-        markingType={"period"}
-      />
-      <TextInput
-        label="Start Date"
-        value={startDate ? startDate.dateString : ""}
-        editable={false}
-      />
-      <TextInput
-        label="End Date"
-        value={endDate ? endDate.dateString : ""}
-        editable={false}
-      />
+    <View style={styles.container}>
+      <Text style={styles.heading}>Enter Available Dates</Text>
+      <View style={styles.dateContainer}>
+        <DateTimePicker
+          value={fromDate}
+          minimumDate={new Date()}
+          maximumDate={new Date(2025, 1, 1)}
+          onChange={(event, date) => setFromDate(date)}
+        />
+        <DateTimePicker
+          value={untilDate}
+          minimumDate={fromDate}
+          maximumDate={new Date(2025, 1, 1)}
+          onChange={(event, date) => setUntilDate(date)}
+        />
+      </View>
+      <View style={styles.addButtonContainer}>
+        <Button title="Add Date" onPress={handleAddDate} />
+      </View>
+      <View style={styles.dateListContainer}>
+        <Text style={styles.heading}>Your Listed Dates:</Text>
+        {dateListItems}
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginHorizontal: 20,
+    marginTop: 20,
+  },
+  heading: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+  addButtonContainer: {
+    marginTop: 10,
+  },
+  dateListContainer: {
+    marginTop: 20,
+  },
+  dateListItem: {
+    padding: 10,
+    borderWidth: 1,
+    borderRadius: 5,
+    borderColor: "#ccc",
+    marginBottom: 10,
+  },
+  dateText: {
+    marginBottom: 5,
+  },
+  dateContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+});
 
 export default UpdateListHouse;

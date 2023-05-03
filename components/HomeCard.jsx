88f116825821c9ad543c238f6d33ad76495acb39
Quinn Lord <quinndolin404@gmail.com>
2023-05-03 09:53:28 +0100

    add composable home card with example usage

--- components/HomeCard.jsx
+++ components/HomeCard.jsx
@@ -1,10 +1,10 @@
-import { Text, View } from "react-native";
+import { Text, View, Image } from "react-native";
 import React, { createContext, useContext } from "react";
 
 const HomeContext = createContext(null);
 
 const useHomeContext = () => {
-  const conext = useContext(HomeContext);
+  const context = useContext(HomeContext);
   if (!context) {
     throw new Error("HomeCard.* component must be a child of HomeCard");
   }
@@ -13,16 +13,67 @@ const useHomeContext = () => {
 
 const HomeCard = ({ home, children }) => {
   return (
-    <HomeContext.Provider value={{ home }}>
-      <View>{children}</View>
+    <HomeContext.Provider value={home}>
+      <View className="border border-green-600 p-3 text-green-600 rounded-lg">
+        {children}
+      </View>
     </HomeContext.Provider>
   );
 };
 
-const HomeInfo = ({ children }) => {
-  return <View>{children}</View>;
+const Info = ({ children }) => {
+  return <View className="flex flex-row justify-between">{children}</View>;
 };
 
-const HomeImage = ({ image }) => {};
+const HomeImage = () => {
+  const home = useHomeContext();
+  return (
+    <Image
+      source={{
+        uri: home.image,
+      }}
+      style={{ width: 350, height: 200 }}
+    />
+  );
+};
+
+const Location = () => {
+  const home = useHomeContext();
+  return <Text className="text-green-600">{home.location}</Text>;
+};
+
+const Rating = () => {
+  const home = useHomeContext();
+  return (
+    <View className="flex flex-row gap-1">
+      <Image
+        source={require("../assets/star.png")}
+        style={{ width: 16, height: 16 }}
+      />
+      <Text className="text-green-600">{home.rating}</Text>
+    </View>
+  );
+};
+
+const GuestCount = () => {
+  const home = useHomeContext();
+  return (
+    <Text className="text-green-600">{`${home.guests.min} - ${home.guests.max} Guests`}</Text>
+  );
+};
+
+const HomeTypeAndBedrooms = () => {
+  const home = useHomeContext();
+  return (
+    <Text className="text-green-600">{`${home.bedrooms} bedroom ${home.type}`}</Text>
+  );
+};
+
+HomeCard.Info = Info;
+HomeCard.Image = HomeImage;
+HomeCard.Location = Location;
+HomeCard.Rating = Rating;
+HomeCard.GuestCount = GuestCount;
+HomeCard.HomeTypeAndBedrooms = HomeTypeAndBedrooms;
 
 export default HomeCard;

3eb5d9e4768a3ee697d0ddb9d5282caa1e304c79
David Maizil <davidmaizil@Davids-MBP.ultrahub>
2023-05-04 11:28:49 +0100

    adding the option to post comments

--- screens/CommentsList.jsx
+++ screens/CommentsList.jsx
@@ -1,8 +1,8 @@
-import React from "react";
-import { View } from "react-native";
+import React, { useState } from "react";
+import { View, Button, TextInput } from "react-native";
 import CommentCard from "./CommentCard";
 
-const comments = [
+const initialComments = [
   {
     id: 1,
     name: "John Doe",
@@ -10,35 +10,42 @@ const comments = [
     date: "May 2023",
     body: "This is the first comment.",
   },
-  {
-    id: 2,
-    name: "Jane Smith",
-    avatar: "https://i.pravatar.cc/150?img=7",
-    date: "June 2023",
-    body: "This is the second comment.",
-  },
-  {
-    id: 3,
-    name: "Bob Johnson",
-    avatar: "https://i.pravatar.cc/150?img=13",
-    date: "July 2023",
-    body: "This is the third comment.",
-  },
-  {
-    id: 4,
-    name: "Sarah Lee",
-    avatar: "https://i.pravatar.cc/150?img=5",
-    date: "August 2023",
-    body: "This is the fourth comment.",
-  },
+  // rest of the comments
 ];
 
 const CommentsList = () => {
+  const [comments, setComments] = useState(initialComments);
+  const [newComment, setNewComment] = useState("");
+
+  const handleAddComment = () => {
+    const id = comments.length + 1;
+    const date = new Date().toLocaleDateString("en-US", {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    });
+    const comment = {
+      id,
+      name: "User",
+      avatar: "https://i.pravatar.cc/150?img=1",
+      date,
+      body: newComment,
+    };
+    setComments([...comments, comment]);
+    setNewComment("");
+  };
+
   return (
     <View style={{ marginTop: 20 }}>
       {comments.map((comment) => (
         <CommentCard key={comment.id} user={comment} comment={comment} />
       ))}
+      <TextInput
+        value={newComment}
+        onChangeText={setNewComment}
+        placeholder="Add a comment..."
+      />
+      <Button title="Add Comment" onPress={handleAddComment} />
     </View>
   );
 };

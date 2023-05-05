0068742202fed9e01e5b6430a6bfb6e746ccaad0
adaml98 <47745325+adaml98@users.noreply.github.com>
2023-05-05 14:51:46 +0100

    implement retrieval of data from firestore to user profile

--- screens/UserProfile.jsx
+++ screens/UserProfile.jsx
@@ -1,20 +1,40 @@
-import { Text, View, Image } from 'react-native';
-import React, { useContext } from 'react';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { Button } from 'react-native-paper';
-import { useAuth } from '../contexts/AuthContext';
-import { LoginContext } from '../contexts/LoggedInContext';
+import { Text, View, Image } from "react-native";
+import React, { useContext, useEffect, useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Button } from "react-native-paper";
+import { useAuth } from "../contexts/AuthContext";
+import { LoginContext } from "../contexts/LoggedInContext";
+import { database } from "../config/firebase";
+
+import { collection, doc, setDoc, getDoc } from "firebase/firestore";
 
 function UserProfile() {
   const { currentUser, setCurrentUser } = useAuth();
   const [isLoggedIn, setIsLoggedIn] = useContext(LoginContext);
+  const [userProfile, setUserProfile] = useState();
+
+  async function getUserProfile(id) {
+    const docRef = doc(database, "userProfilesV2", id);
+    const docSnap = await getDoc(docRef);
+    const results = docSnap.data();
+    // console.log(results);
+    return results;
+  }
+  useEffect(() => {
+    getUserProfile("zzzzzzzzzzzzzzzzzzzy")
+      .then((data) => {
+        setUserProfile(data);
+      })
+      .catch((e) => console.log(e));
+  }, []);
+
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <View
         style={{
           flex: 1,
-          justifyContent: 'space-between',
-          alignItems: 'center',
+          justifyContent: "space-between",
+          alignItems: "center",
         }}
       >
         <View>
@@ -23,7 +43,7 @@ function UserProfile() {
             modeValue="contained"
             title="Logout"
             onPress={() => {
-              setCurrentUser('');
+              setCurrentUser("");
               setIsLoggedIn(false);
             }}
           >
@@ -32,45 +52,45 @@ function UserProfile() {
         </View>
         <View
           style={{
-            width: '90%',
-            flexDirection: 'row',
-            justifyContent: 'flex-end',
-            alignItems: 'center',
+            width: "90%",
+            flexDirection: "row",
+            justifyContent: "flex-end",
+            alignItems: "center",
           }}
         >
           <Button
             style={{
               marginRight: 70,
-              width: '40%',
+              width: "40%",
             }}
             mode="contained"
             buttonColor="#39C67F"
-            onPress={() => console.log('Pressed')}
+            onPress={() => console.log("Pressed")}
           >
             List My House
           </Button>
           <Button
             style={{
-              width: '40%',
+              width: "40%",
             }}
             mode="contained"
             buttonColor="#39C67F"
-            onPress={() => console.log('Pressed')}
+            onPress={() => console.log("Pressed")}
           >
             Edit Profile
           </Button>
         </View>
         <View
           style={{
-            width: '100%',
-            flexDirection: 'row',
-            justifyContent: 'space-around',
-            alignItems: 'center',
+            width: "100%",
+            flexDirection: "row",
+            justifyContent: "space-around",
+            alignItems: "center",
           }}
         >
-          <View style={{ textAlign: 'center' }}>
+          <View style={{ textAlign: "center" }}>
             <Image
-              source={require('../imagesTemp/avatar.png')}
+              source={require("../imagesTemp/avatar.png")}
               style={{
                 width: 100,
                 height: 100,
@@ -78,14 +98,14 @@ function UserProfile() {
               }}
             />
           </View>
-          <View style={{ alignItems: 'center' }}>
-            <Text>Name</Text>
-            <Text>Location</Text>
-            <Text>Rating</Text>
+          <View style={{ alignItems: "center" }}>
+            <Text>{userProfile.firstName + " " + userProfile.lastName}</Text>
+            <Text>{userProfile.Location}</Text>
+            <Text>{userProfile.Rating + " *"}</Text>
             <Button
               mode="contained"
               buttonColor="#39C67F"
-              onPress={() => console.log('Pressed')}
+              onPress={() => console.log("Pressed")}
             >
               Contact Me
             </Button>
@@ -93,18 +113,18 @@ function UserProfile() {
         </View>
         <View
           style={{
-            width: '90%',
+            width: "90%",
             height: 100,
-            borderWidth: '2',
+            borderWidth: "2",
             borderRadius: 10,
           }}
         >
-          <Text>Bio</Text>
+          <Text>{userProfile.Bio}</Text>
         </View>
         <View>
           <Text>House Pictures</Text>
           <Image
-            source={require('../imagesTemp/house.jpg')}
+            source={require("../imagesTemp/house.jpg")}
             style={{
               width: 370,
               height: 200,
@@ -113,9 +133,9 @@ function UserProfile() {
         </View>
         <View
           style={{
-            width: '90%',
+            width: "90%",
             height: 100,
-            borderWidth: '2',
+            borderWidth: "2",
             borderRadius: 10,
           }}
         >

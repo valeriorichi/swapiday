564fca6a0b25e0c55c05ae7ee1b72b09b0417415
David Maizil <davidmaizil@Davids-MBP.ultrahub>
2023-05-05 10:42:45 +0100

    connected to  db

--- screens/WishList.jsx
+++ screens/WishList.jsx
@@ -1,6 +1,7 @@
 import { ScrollView, Text } from "react-native";
 import LogoHeader from "./LogoHeader";
 import { database, auth } from "../config/firebase";
+import "firebase/firestore";
 import {
   collection,
   addDoc,
@@ -8,8 +9,19 @@ import {
   query,
   onSnapshot,
 } from "firebase/firestore";
+import { useEffect } from "react";
 
 function WishList() {
+  useEffect(() => {
+    const todosRef = collection(database, "todos");
+    const unsubscribe = onSnapshot(todosRef, (querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        console.log(doc.id, doc.data());
+      });
+    });
+    return () => unsubscribe();
+  }, []);
+
   return (
     <ScrollView>
       <LogoHeader />
